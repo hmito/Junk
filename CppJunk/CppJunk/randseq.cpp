ce56cac8e0f860a850919c5c34601d2e0d4374a0
yumetodo <yume-wikijp@live.jp>
2015-09-29 21:36:58 +0900

    sortしない版を追加。

--- CppJunk/CppJunk/randseq.cpp
+++ CppJunk/CppJunk/randseq.cpp
@@ -11,20 +11,26 @@ void print_elapsed_time(const char* str, T start, T end){
 		<< " msec."
 		<< std::endl;
 }
-using make_rand_array_type = std::vector<int>(*)(size_t, int, int);
 int main(){
 	using clock = std::chrono::high_resolution_clock;
 	size_t array_num = 10000;
 	int rand_max = 1000000;
 	int rand_min = -1000000;
 
-	std::vector<std::pair<std::string, std::function<void(size_t, int, int)>>> FuncArray;
+	std::vector<std::pair<std::string, std::function<std::vector<int>(size_t, int, int)>>> FuncArray;
 
-	FuncArray.push_back(std::make_pair("make_rand_array_hash", make_rand_array_hash));
-	FuncArray.push_back(std::make_pair("make_rand_array_unique", make_rand_array_unique));
-//	FuncArray.push_back(std::make_pair("make_rand_array_shuffle", make_rand_array_shuffle));
+	FuncArray.emplace_back("make_rand_array_hash", make_rand_array_hash);
+	FuncArray.emplace_back("make_rand_array_unique", make_rand_array_unique);
+	//FuncArray.emplace_back("make_rand_array_shuffle", make_rand_array_shuffle);
+	FuncArray.emplace_back("make_rand_array_liner_gen_shuffle", make_rand_array_liner_gen_shuffle);
 
 	try{
+		{
+			const auto a = make_rand_array_liner_gen_shuffle(24, 150, -150);
+			for (auto i : a) std::cout << i << ", ";
+			std::cout << std::endl << std::endl;
+			system("pause");
+		}
 		for(auto val : {10,100,1000,10000}){
 			array_num = 10000;
 			rand_max = val*array_num;
--- CppJunk/CppJunk/randseq.hpp
+++ CppJunk/CppJunk/randseq.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#
+
 #include <random>
 #include <vector>
 #include <iostream>
@@ -8,6 +8,8 @@
 #include <stdexcept>
 #include <unordered_set>
 #include <functional>
+#include <limits>
+#include <type_traits>
 
 std::mt19937 create_rand_engine(){
 	std::random_device rnd;
@@ -45,7 +47,7 @@ std::vector<int> make_rand_array_unique(const size_t size, int rand_min, int ran
 		std::sort(tmp.begin(), tmp.end());
 		auto unique_end = std::unique(tmp.begin(), tmp.end());
 
-		if(size < std::distance(tmp.begin(), unique_end)){
+		if(size < static_cast<size_t>(std::distance(tmp.begin(), unique_end))){
 			unique_end = std::next(tmp.begin(), size);
 		}
 		tmp.erase(unique_end, tmp.end());
@@ -72,4 +74,43 @@ std::vector<int> make_rand_array_shuffle(const size_t size, int rand_min, int ra
 
 	return std::move(tmp);
 }
-
+namespace detail {
+	template<typename Re>class affluence_inc_gen_c {
+	public:
+		affluence_inc_gen_c(uintmax_t affluence) : affluence_(affluence) {}
+		Re operator()(std::mt19937& engin) {
+			const auto re = std::uniform_int_distribution<Re>(0, this->make_rand_max())(engin);
+			this->affluence_ -= re;
+			return re;
+		}
+	private:
+		Re make_rand_max() { return (this->affluence_ < std::numeric_limits<Re>::max()) ? static_cast<Re>(this->affluence_) : std::numeric_limits<Re>::max(); }
+		uintmax_t affluence_;
+	};
+	template<typename T> auto diff(T n1, T n2) -> typename std::make_unsigned<T>::type {
+		static_assert(std::is_integral<T>::value, "T is not integral.");
+		if (n1 < n2) std::swap(n1, n2);
+		return static_cast<typename std::make_unsigned<T>::type>(n1 - n2);
+	}
+}
+std::vector<int> make_rand_array_liner_gen_shuffle(const size_t size, int rand_min, int rand_max) {
+	using type = decltype(rand_min);
+	if (rand_min > rand_max) std::swap(rand_min, rand_max);
+	const auto max_min_diff = detail::diff(rand_max, rand_min) + 1;
+	if (max_min_diff < size) throw std::runtime_error("ˆø”‚ªˆÙí‚Å‚·");
+	const auto affluence = max_min_diff - size;
+	auto engine = create_rand_engine();
+	if (0 == affluence) return [size, rand_min, &engine]() {
+		std::vector<type> re(size);
+		int t = rand_min;
+		for (auto& i : re) i = t++;
+		std::shuffle(re.begin(), re.end(), engine);
+		return re;
+	}();
+	std::vector<type> re;
+	re.reserve(size);
+	detail::affluence_inc_gen_c<type> affluence_inc(affluence);
+	for (auto i = affluence_inc(engine) + rand_min; re.size() < size; i += affluence_inc(engine) + 1) re.push_back(i);
+	std::shuffle(re.begin(), re.end(), engine);
+	return std::move(re);
+}
