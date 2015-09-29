9d917b19cb3aad49cc0be291861403e616b9efe7
hmito <hmito@outlook.com>
2015-09-29 15:11:41 +0900

    Add randseq for check performance for creating random sequence without duplication.

---
+++ CppJunk/CppJunk/randseq.cpp
@@ -0,0 +1,52 @@
+#include<chrono>
+#include<vector>
+#include<array>
+#include<utility>
+#include"randseq.hpp"
+
+template<class T>
+void print_elapsed_time(const char* str, T start, T end){
+	std::cout << str << " : "
+		<< std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+		<< " msec."
+		<< std::endl;
+}
+using make_rand_array_type = std::vector<int>(*)(size_t, int, int);
+int main(){
+	using clock = std::chrono::high_resolution_clock;
+	size_t array_num = 10000;
+	int rand_max = 1000000;
+	int rand_min = -1000000;
+
+	std::vector<std::pair<std::string, std::function<void(size_t, int, int)>>> FuncArray;
+
+	FuncArray.push_back(std::make_pair("make_rand_array_hash", make_rand_array_hash));
+	FuncArray.push_back(std::make_pair("make_rand_array_unique", make_rand_array_unique));
+	FuncArray.push_back(std::make_pair("make_rand_array_shuffle", make_rand_array_shuffle));
+
+	try{
+		for(auto val : {1,2,4,8,16}){
+			array_num = 100000;
+			rand_max = val*array_num;
+			rand_min = -val*array_num;
+
+			std::cout << "array_num : " << array_num << "rand_max : " << rand_max << "rand_min : " << rand_min << std::endl;
+			for(auto func : FuncArray){
+				const auto t0 = clock::now();
+
+				func.second(array_num, rand_min, rand_max);
+
+				print_elapsed_time(func.first.c_str(), t0, clock::now());
+			}
+		}
+	}
+	catch(const std::exception& er){
+		std::cerr << er.what() << std::endl;
+	}
+
+
+	system("pause");
+
+	return 0;
+}
+
---
+++ CppJunk/CppJunk/randseq.hpp
@@ -0,0 +1,75 @@
+#pragma once
+#
+#include <random>
+#include <vector>
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+#include <stdexcept>
+#include <unordered_set>
+#include <functional>
+
+std::mt19937 create_rand_engine(){
+	std::random_device rnd;
+	std::vector<std::uint_least32_t> v(10);// 初期化用ベクタ
+	std::generate(v.begin(), v.end(), std::ref(rnd));// ベクタの初期化
+	std::seed_seq seed(v.begin(), v.end());
+	return std::mt19937(seed);// 乱数エンジン
+}
+
+std::vector<int> make_rand_array_hash(const size_t size, int rand_min, int rand_max){
+	if(rand_min > rand_max) std::swap(rand_min, rand_max);
+	const size_t max_min_diff = static_cast<size_t>(rand_max - rand_min + 1);
+	if(max_min_diff < size) throw std::runtime_error("引数が異常です");
+
+	std::unordered_set<int> tmp;
+	auto engine = create_rand_engine();
+	std::uniform_int_distribution<int> distribution(rand_min, rand_max);
+	while(tmp.size() < size) tmp.insert(distribution(engine));
+	return std::vector<int>(tmp.begin(), tmp.end());
+}
+
+std::vector<int> make_rand_array_unique(const size_t size, int rand_min, int rand_max){
+	if(rand_min > rand_max) std::swap(rand_min, rand_max);
+	const size_t max_min_diff = static_cast<size_t>(rand_max - rand_min + 1);
+	if(max_min_diff < size) throw std::runtime_error("引数が異常です");
+
+	std::vector<int> tmp;
+	auto engine = create_rand_engine();
+	std::uniform_int_distribution<int> distribution(rand_min, rand_max);
+
+	const size_t make_size = static_cast<size_t>(size*1.2);
+
+	while(tmp.size() < size){
+		while(tmp.size() < make_size) tmp.push_back(distribution(engine));
+		std::sort(tmp.begin(), tmp.end());
+		auto unique_end = std::unique(tmp.begin(), tmp.end());
+
+		if(size < std::distance(tmp.begin(), unique_end)){
+			unique_end = std::next(tmp.begin(), size);
+		}
+		tmp.erase(unique_end, tmp.end());
+	}
+
+	std::shuffle(tmp.begin(), tmp.end(), engine);
+	return std::move(tmp);
+}
+
+std::vector<int> make_rand_array_shuffle(const size_t size, int rand_min, int rand_max){
+	if(rand_min > rand_max) std::swap(rand_min, rand_max);
+	const size_t max_min_diff = static_cast<size_t>(rand_max - rand_min + 1);
+	if(max_min_diff < size) throw std::runtime_error("引数が異常です");
+
+	std::vector<int> tmp;
+	tmp.reserve(max_min_diff);
+
+	for(int i = rand_min; i <= rand_max; ++i)tmp.push_back(i);
+
+	auto engine = create_rand_engine();
+	std::shuffle(tmp.begin(), tmp.end(), engine);
+
+	tmp.erase(std::next(tmp.begin(), size), tmp.end());
+
+	return std::move(tmp);
+}
+
